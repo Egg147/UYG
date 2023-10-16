e245dd54fbdd1ba89514d5fd18d94967d195597c
USER <babacanege6@gmail.com>
2023-10-16 12:17:16 +0300

    uyg

---
+++ app/src/main/java/DenemeTamSayılar.java
@@ -0,0 +1,12 @@
+public class DenemeTamSayılar {
+    public static void main(String[] args) {
+byte kucukSayi = 127;
+short KisaSayi = 32767;
+int TamSayi = 2147483647;
+long   UzunSayi = 9223372036854775807L;
+        System.out.println("byte: "+ kucukSayi);
+        System.out.println("short: " + KisaSayi);
+        System.out.println("int: " + TamSayi);
+        System.out.println("long: " + UzunSayi);
+    }
+}
